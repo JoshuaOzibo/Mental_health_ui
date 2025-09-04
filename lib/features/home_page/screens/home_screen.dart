029ef8fccced56e3550926e11f5ca2d81c369242
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-09-04 13:01:52 +0100

    Refactor HomeScreen layout and enhance notification button design

--- lib/features/home_page/screens/home_screen.dart
+++ lib/features/home_page/screens/home_screen.dart
@@ -10,12 +10,17 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: NovaColors.backgroundLight,
       appBar: AppBar(
+        leadingWidth: 300,
+        toolbarHeight: 80,
+        backgroundColor: NovaColors.backgroundLight,
         leading: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
-              margin: EdgeInsets.only(left: 16),
-              width: 40,
-              height: 40,
+              margin: EdgeInsets.only(left: 16, right: 8),
+              width: 60,
+              height: 60,
               decoration: BoxDecoration(
                 color: NovaColors.green,
                 shape: BoxShape.circle,
@@ -24,6 +29,8 @@ class HomeScreen extends StatelessWidget {
             ),
 
             Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   'Hi, Einstein',
@@ -41,33 +48,38 @@ class HomeScreen extends StatelessWidget {
         actions: [
           Container(
             margin: EdgeInsets.only(right: 16),
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            // padding: EdgeInsets.symmetric(horizontal: 13, vertical: 18),
+            width: 55,
+            height: 60,
             decoration: BoxDecoration(
               border: Border.all(color: NovaColors.iconGray),
               borderRadius: BorderRadius.circular(12),
             ),
-            child: Stack(
-              children: [
-                Icon(Icons.notifications, color: NovaColors.iconGray),
-                Positioned(
-                  right: 0,
-                  top: 0,
-                  child: Container(
-                    width: 8,
-                    height: 8,
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      shape: BoxShape.circle,
-                    ),
-
-                    child: Text(
-                      '3',
-                      style: TextStyle(fontSize: 6, color: Colors.white),
-                      textAlign: TextAlign.center,
+            child: Center(
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Icon(Icons.notifications_none_outlined, size: 25),
+                  Positioned(
+                    right: 1,
+                    top: -3,
+                    child: Container(
+                      width: 15,
+                      height: 15,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 227, 15, 0),
+                        shape: BoxShape.circle,
+                      ),
+              
+                      child: Text(
+                        '3',
+                        style: TextStyle(fontSize: 10, color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'features/home_page/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: const HomeScreen(),
+    );
   }
 }
