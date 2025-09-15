2d7394855ace01c007cbf812db5bb7bae74a8ced
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-09-15 15:30:57 +0100

    feat: add practiceData list for meditation and mindfulness practices

--- lib/core/static/data.dart
+++ lib/core/static/data.dart
@@ -24,3 +24,32 @@ List<Map<String, String>> data = [
     "image": "assets/images/pexels-pixabay-158063.jpg",
   },
 ];
+
+List<Map<String, String>> practiceData = [
+  {
+    "practice" : "Meditation",
+    "time" : "Meditation . 10 mins",
+    "color": "",
+    
+  },
+  {
+    "practice" : "Positive focus",
+    "time" : "Meditation . 10 mins",
+  },
+  {
+    "practice" : "Meditation",
+    "time" : "Positive focus . 15 mins",
+  },
+  {
+    "practice" : "Wake up",
+    "time" : "Wake up . 6 hours",
+  },
+  {
+    "practice" : "Mindfulness . 15 mins",
+    "time" : "Mindfulness . 15 mins",
+  },
+  {
+    "practice" : "Wake up",
+    "time" : "Wake up . 6 hours",
+  },
+];
